0f7549f0ebbcad32cc3b145458ca784aa184bc3f
Shane Livingston <rshane7@gmail.com>
2020-03-14 17:56:30 -0500

    updates

--- Instructions/app.py
+++ Instructions/app.py
@@ -1,64 +1,37 @@
 from flask import Flask, render_template, redirect
-<<<<<<< HEAD
 from flask_pymongo import PyMongo
-=======
-from flask_pymongo import pymongo
-from pymongo import MongoClient
-
->>>>>>> ef54b21c2a262736030a760f090c5938f7a5bbc9
-
-# From the separate python file in this directory, we'll import the code that is used to scrape mars
 import scrape_mars
+print('1 - imports')
 
+# Create an instance of Flask
 app = Flask(__name__)
+print('2 = flask-name')
 
-# Use flask_pymongo to set up mongo connection
-<<<<<<< HEAD
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
-mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+print('3 - MongoDB')
 
-# set database (used this first time to create db)
-#db=client.mars_db
-
-#conn = 'mongodb://localhost:27017'
-#client = pymongo.MongoClient(conn)
-=======
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
-# mongo = PyMongo(app)
-
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn)
-
-# set database (used this first time to create db)
-db=client.mars_db
->>>>>>> ef54b21c2a262736030a760f090c5938f7a5bbc9
-
-# This route will query the Mongo database and get the mars result
-# and then render them to the html file
-
+# Route to render index.html template using data from Mongo
 @app.route("/")
 def home():
-    
-    mars_results = db.mars_results.find()
-    return render_template("index.html", mars_results=mars_results)
+    # Find one record of data from the mongo database
+    mars_data = mongo.db.mars_scrape.find_one()
+    # Return template and data
+    return render_template("index.html", mars_data=mars_data)
+print('4 - route /')
 
-# This route will trigger the webscraping, but it will then send us 
-# back to the index route to render the results
-
+# Route that will trigger the scrape function
 @app.route("/scrape")
-def scraper():
-# drop any data that is already in database
-    db.mars_results.drop()
+def scrape():
+    # Run the scrape function
     mars_data = scrape_mars.scrape()
-    
-    db.mars_results.insert_one(mars_data)
-    
-    # Use Flask's redirect function to send us to a different route once this task has completed.
+    mars_data
+    # Update the Mongo database using update and upsert=True
+    mongo.db.scrape_mars.update({}, mars_data, upsert=True)
+    # Redirect back to home page
     return redirect("/")
+print('5 - route scrape')
 
-<<<<<<< HEAD
 if __name__ == "__main__":
     app.run(debug=True)
-=======
-if __name__ == "__main__":
->>>>>>> ef54b21c2a262736030a760f090c5938f7a5bbc9
+print('6 - debug')
